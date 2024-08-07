d5241a5f903d865626b34a01f744f57da75c8bfa
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-06 21:59:48 -0300

    adicionado os campos de registro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,30 +18,59 @@ export default function Home() {
 
       <section className="flex flex-col items-center w-full space-y-7 ">
         <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48"/>
+          <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48" />
         </svg>
         <h1 className="text-4xl font-extrabold uppercase">Sphere</h1>
 
         <Tabs defaultValue="entrar">
-          <TabsList>
+          <TabsList className="flex items-center w-full">
             <TabsTrigger value="entrar">entrar</TabsTrigger>
             <TabsTrigger value="registrar">registrar</TabsTrigger>
           </TabsList>
 
           <TabsContent value="entrar">
             <form action="" className="space-y-2">
-              <Label htmlFor="email">email</Label>
-              <Input id="email" />
-              <Label htmlFor="senha">senha</Label>
-              <Input id="senha" type="password" />
+              <div>
+                <Label htmlFor="email">email</Label>
+                <Input id="email" />
+              </div>
+
+              <div>
+                <Label htmlFor="senha">senha</Label>
+                <Input id="senha" type="password" />
+              </div>
+
               <Button type="submit">entrar</Button>
             </form>
           </TabsContent>
-          
+
           <TabsContent value="registrar">
-            <p>criar conta</p>
+            <form action="" className="space-y-2 ">
+              <div>
+                <Label htmlFor="nome" className="">nome</Label>
+                <Input id="nome" />
+              </div>
+
+              <div>
+                <Label htmlFor="bio">bio</Label>
+                <Input id="bio" />
+              </div>
+
+              <div>
+                <Label htmlFor="email">email</Label>
+                <Input id="email" type="email" />
+              </div>
+
+              <div>
+                <Label htmlFor="senha">senha</Label>
+                <Input id="senha" type="password" />
+              </div>
+              <div className="flex flex-col items-center w-full">
+                <Button type="submit">criar conta</Button>
+              </div>
+            </form>
           </TabsContent>
-        
+
         </Tabs>
 
 
